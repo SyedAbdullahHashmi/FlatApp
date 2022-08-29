a92a3f53289380ba981f5e991bf3c390ffff7832
AbD-hashmi <abdullah.hashmi24@gmail.com>
2022-08-29 13:41:25 +0530

    initial commit

---
+++ app/src/main/java/com/my/assignment/utilities/AppNameGlideModule.kt
@@ -0,0 +1,13 @@
+package com.my.assignment.utilities
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+
+//Glide Module
+@GlideModule
+class AssignmentGlideModule : AppGlideModule() {
+
+
+}
